96461ab77ab987958799edcaae80ef0bd40f517a
Georg Wiese <georgwiese@gmail.com>
2022-09-03 17:23:10 +0200

    Implement Merkle Tree

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use hash_based_signatures::MerkleTree;
+
 fn main() {
-    println!("Hello, world!");
+    let element = [0u8; 32];
+    let elements = [element, element, element, element];
+    let tree = MerkleTree::new(&elements);
+    let root_hash = tree.root();
+
+    for i in root_hash {
+        println!("{}", i);
+    }
 }
