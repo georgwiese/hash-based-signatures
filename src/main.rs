27aae225b05065e0cbc413bc20011cc30ac34bc6
Georg Wiese <georgwiese@gmail.com>
2022-09-20 23:55:11 +0200

    Implement verification

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use hash_based_signatures::merkle_tree::{MerkleProof, MerkleTree};
 use hash_based_signatures::signature::stateless_merkle::{
     StatelessMerklePrivateKey, StatelessMerkleSignatureScheme,
 };
@@ -8,13 +7,18 @@ use hmac_sha256::Hash;
 use rand;
 use rand::RngCore;
 use rmp_serde;
-use serde::Deserializer;
-use std::fs;
-use std::fs::File;
-use std::io::{Read, Write};
-use std::path::{Path, PathBuf};
 
-fn keygen(width: usize, depth: usize) {
+use std::fs;
+
+use std::path::PathBuf;
+
+fn keygen(width: usize, depth: usize) -> HashType {
+    println!();
+    println!(" #######################");
+    println!("   Generating key");
+    println!(" #######################");
+    println!();
+
     let mut seed = [0u8; 32];
     let mut rng = rand::thread_rng();
     rng.fill_bytes(&mut seed);
@@ -24,13 +28,21 @@ fn keygen(width: usize, depth: usize) {
 
     let private_key_json = serde_json::to_string(&private_key).unwrap();
     let output_path = ".private_key.json";
-    fs::write(output_path, private_key_json);
+    fs::write(output_path, private_key_json).expect("Could not write private key");
 
     println!("Public key:       {}", hash_to_string(&public_key));
     println!("Private key path: {}", output_path);
+
+    public_key
 }
 
 fn sign(path: PathBuf) {
+    println!();
+    println!(" #######################");
+    println!("   Signing File");
+    println!(" #######################");
+    println!();
+
     let data = fs::read(&path).expect("Unable to read file");
     let private_key_json = fs::read_to_string(".private_key.json").unwrap();
     let private_key = serde_json::from_str(&private_key_json).unwrap();
@@ -38,7 +50,7 @@ fn sign(path: PathBuf) {
     let file_hash = Hash::hash(&data);
     let mut signature_scheme = StatelessMerkleSignatureScheme::from_private_key(&private_key);
 
-    println!("Signing file:   {}", &path.to_str().unwrap());
+    println!("File Path:      {}", &path.to_str().unwrap());
     println!("Hash:           {}", hash_to_string(&file_hash));
     println!(
         "Public key:     {}",
@@ -53,7 +65,31 @@ fn sign(path: PathBuf) {
     fs::write(output_path, &signature_bytes).expect("Could not write signature");
 }
 
+fn verify(file_path: PathBuf, signature_path: PathBuf, public_key: HashType) {
+    println!();
+    println!(" #######################");
+    println!("   Verifying file");
+    println!(" #######################");
+    println!();
+
+    let data = fs::read(&file_path).expect("Unable to read file");
+    let file_hash = Hash::hash(&data);
+
+    let signature = rmp_serde::from_slice(&fs::read(&signature_path).unwrap()).unwrap();
+
+    let verifies = StatelessMerkleSignatureScheme::verify(public_key, file_hash, &signature);
+
+    println!("File Path:      {}", &file_path.to_str().unwrap());
+    println!("Signature Path: {}", &signature_path.to_str().unwrap());
+    println!("Valid:          {}", verifies);
+}
+
 fn main() {
-    keygen(16, 5);
+    let public_key = keygen(16, 5);
     sign(PathBuf::from("Cargo.toml"));
+    verify(
+        PathBuf::from("Cargo.toml"),
+        PathBuf::from("Cargo.toml.signature"),
+        public_key,
+    );
 }
