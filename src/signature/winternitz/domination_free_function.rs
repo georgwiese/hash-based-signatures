e183f2751a25d8648796f84652f92ef0f9bdd4fa
Georg Wiese <georgwiese@gmail.com>
2022-09-23 11:46:10 +0200

    Add tests

--- src/signature/winternitz/domination_free_function.rs
+++ src/signature/winternitz/domination_free_function.rs
@@ -68,6 +68,8 @@ pub fn domination_free_function(input: HashType, d: u64) -> Vec<u8> {
 #[cfg(test)]
 mod tests {
     use crate::signature::winternitz::domination_free_function::domination_free_function;
+    use rand::prelude::*;
+    use rand_chacha::ChaCha20Rng;
 
     #[test]
     fn test_domination_free_function_0s_d1() {
@@ -102,8 +104,87 @@ mod tests {
         // bits_to_combine is 4
         // Maximal value of c is 15 * 64 = 0x3c0
         // Which should be encoded in 3 4-bit integers
-        expected.extend([3, 0xc, 0]);
+        expected.extend([0x3, 0xc, 0x0]);
 
         assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_domination_free_function_0s_d255() {
+        let result = domination_free_function([0; 32], 255);
+        let mut expected = vec![0u8; 32];
+
+        // bits_to_combine is 4
+        // Maximal value of c is 255 * 32 = 0x1FE0
+        // Which should be encoded in 2 16-bit integers
+        expected.extend([0x1f, 0xe0]);
+
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_domination_free_function_1s_d255() {
+        let result = domination_free_function([255; 32], 255);
+        let mut expected = vec![255u8; 32];
+
+        // bits_to_combine is 4
+        // Maximal value of c is 255 * 32 = 0x1FE0
+        // Which should be encoded in 2 16-bit integers
+        expected.extend([0, 0]);
+
+        assert_eq!(result, expected);
+    }
+
+    fn get_domination_free_vectors_on_random_data(d: u64, count: usize) -> Vec<Vec<u8>> {
+        let mut rng = ChaCha20Rng::from_seed([0; 32]);
+        let mut hash = [0u8; 32];
+        (0..count)
+            .map(|_| {
+                rng.fill_bytes(&mut hash);
+                domination_free_function(hash, d)
+            })
+            .collect()
+    }
+
+    fn assert_no_domination(domination_free_vectors: &Vec<Vec<u8>>) {
+        for i in 0..domination_free_vectors.len() {
+            for j in 0..domination_free_vectors.len() {
+                if i != j {
+                    // The two vectors should be distinct with overwhelming probability and
+                    // at least one value should of vec1 should be higher than the corresponding
+                    // value in vec2.
+                    let vec1 = &domination_free_vectors[i];
+                    let vec2 = &domination_free_vectors[j];
+
+                    let v1_dominates_sometimes =
+                        vec1.iter().zip(vec2).map(|(v1, v2)| v1 > v2).max().unwrap();
+                    assert!(v1_dominates_sometimes);
+                }
+            }
+        }
+    }
+
+    #[test]
+    fn domination_free_on_random_data_d1() {
+        let domination_free_vectors = get_domination_free_vectors_on_random_data(1, 1000);
+        assert_no_domination(&domination_free_vectors);
+    }
+
+    #[test]
+    fn domination_free_on_random_data_d3() {
+        let domination_free_vectors = get_domination_free_vectors_on_random_data(3, 1000);
+        assert_no_domination(&domination_free_vectors);
+    }
+
+    #[test]
+    fn domination_free_on_random_data_d15() {
+        let domination_free_vectors = get_domination_free_vectors_on_random_data(15, 1000);
+        assert_no_domination(&domination_free_vectors);
+    }
+
+    #[test]
+    fn domination_free_on_random_data_d255() {
+        let domination_free_vectors = get_domination_free_vectors_on_random_data(255, 1000);
+        assert_no_domination(&domination_free_vectors);
     }
 }
