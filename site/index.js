4520e34d14ff5978456c7abf542432786f56284f
Georg Wiese <georgwiese@gmail.com>
2022-11-02 10:49:05 +0100

    Finish basic form

--- site/index.js
+++ site/index.js
@@ -1,3 +1,20 @@
+async function check_signature() {
+    let public_key = document.forms["form"]["public_key"].value;
+    let file_array_buffer = await document.forms["form"]["file"].files[0].arrayBuffer();
+    let signature_array_buffer = await document.forms["form"]["signature"].files[0].arrayBuffer();
+
+    console.log(public_key);
+    console.log(file_array_buffer);
+    console.log(signature_array_buffer);
+}
+
 import("./node_modules/hash-based-signatures/hash_based_signatures.js").then((js) => {
-  js.greet("WebAssembly with npm");
+    function onSubmit() {
+        setTimeout(check_signature, 0);
+
+        // js.greet("WebAssembly with npm");
+
+        return false;
+    }
+    window.onSubmit = onSubmit;
 });
