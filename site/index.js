0caebd9d5af2534d41347a48bcdbb0c0aa91e437
Georg Wiese <georgwiese@gmail.com>
2022-11-02 10:33:29 +0100

    Follow rust WASM tutorial

---
+++ site/index.js
@@ -0,0 +1,3 @@
+import("./node_modules/hash-based-signatures/hash_based_signatures.js").then((js) => {
+  js.greet("WebAssembly with npm");
+});
--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,15 @@ pub mod io;
 pub mod merkle_tree;
 pub mod signature;
 pub mod utils;
+
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern "C" {
+    pub fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet(name: &str) {
+    alert(&format!("Hello, {}!", name));
+}
